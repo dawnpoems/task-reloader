d7de6af9104db78fa9b95463cef5e2689cea5c24
dawnpoems <108113517+dawnpoems@users.noreply.github.com>
2026-03-21 16:45:19 +0900

    feat: update dependencies and ESLint configuration for improved compatibility

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: check check-web check-api
 
-check: check-web check-api
+check: check-api check-web
 
 check-web:
 	cd apps/web && npm ci && npm run type-check && npm run lint && npm run build
---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', 'node_modules'],
+}
